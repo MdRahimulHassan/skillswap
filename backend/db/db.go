6ba39aceea43c0626e143309c128238e281dc337
Jawadun <jawadunnoor@gmail.com>
2025-11-15 03:15:27 +0600

    containarised backend and database with docker

--- backend/db/db.go
+++ backend/db/db.go
@@ -2,17 +2,30 @@ package db
 
 import (
     "database/sql"
+    "fmt"
     "log"
+    "os"
 
     _ "github.com/lib/pq"
+    "github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func Connect() {
-    var err error
-    dsn := "host=localhost port=5432 user=postgres password=1w111bback dbname=skill_swap sslmode=disable"
+    // Load environment variables
+    godotenv.Load()
 
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+
+    dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        host, port, user, password, dbname)
+
+    var err error
     DB, err = sql.Open("postgres", dsn)
     if err != nil {
         log.Fatal(err)
